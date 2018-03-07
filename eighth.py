fd0c60d71ab2be2f2617a38d4be67e62a4e3b27e
XiongMM <xiongmm920@163.com>
2018-03-07 21:15:13 +0800

    函数

---
+++ eighth.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+
+'''
+函数
+'''
+
+# 计算面积函数
+def area(width, height):
+    return width * height
+ 
+def print_welcome(name):
+    print("Welcome", name)
+
+print_welcome("Runoob")
+w = 4
+h = 5
+print("width =", w, " height =", h, " area =", area(w, h))
---
+++ eighth_1.py
@@ -0,0 +1,84 @@
+'''
+参数传递
+
+可更改(mutable)与不可更改(immutable)对象
+在 python 中，strings, tuples, 和 numbers 是不可更改的对象，而 list,dict 等则是可以修改的对象。
+
+不可变类型：变量赋值 a=5 后再赋值 a=10，这里实际是新生成一个 int 值对象 10，再让 a 指向它，而 5 被丢弃，不是改变a的值，相当于新生成了a。
+可变类型：变量赋值 la=[1,2,3,4] 后再赋值 la[2]=5 则是将 list la 的第三个元素值更改，本身la没有动，只是其内部的一部分值被修改了。
+
+python 函数的参数传递：
+不可变类型：类似 c++ 的值传递，如 整数、字符串、元组。如fun（a），传递的只是a的值，没有影响a对象本身。比如在 fun（a）内部修改 a 的值，只是修改另一个复制的对象，不会影响 a 本身。
+可变类型：类似 c++ 的引用传递，如 列表，字典。如 fun（la），则是将 la 真正的传过去，修改后fun外部的la也会受影响
+
+python 中一切都是对象，严格意义我们不能说值传递还是引用传递，我们应该说传不可变对象和传可变对象。
+'''
+#1、传递不可变的对象实例
+def ChangeInt(x):
+    x = 100
+
+b = 2
+ChangeInt(b)
+print(b)
+
+#2、传递可变对象实例
+# 可写函数说明
+def changeme( mylist ):
+   "修改传入的列表"
+   mylist.append([1,2,3,4]);
+   print ("函数内取值: ", mylist)
+   return
+ 
+# 调用changeme函数
+mylist = [10,20,30];
+changeme( mylist );
+print ("函数外取值: ", mylist)
+
+#3、参数
+#3.1、必须参数
+#可写函数说明
+def printme( str ):
+   "打印任何传入的字符串"
+   print (str)
+   return
+ 
+#调用printme函数
+printme("nihao")#这个地方不传入参数会报错
+
+#3.2、关键字参数
+#可写函数说明
+def printinfo( name, age ):
+   "打印任何传入的字符串"
+   print ("名字: ", name)
+   print ("年龄: ", age)
+   return
+ 
+#调用printinfo函数
+printinfo( age=50, name="runoob" ) #可不考虑参数的先后
+
+#3.3、默认参数
+#可写函数说明
+def printinfo( name, age = 35 ):
+   "打印任何传入的字符串"
+   print ("名字: ", name);
+   print ("年龄: ", age);
+   return;
+ 
+#调用printinfo函数
+printinfo( age=50, name="runoob" );
+print ("------------------------")
+printinfo( name="runoob" );
+
+#3.4、不定长参数
+# 可写函数说明
+def printinfo( arg1, *vartuple ):
+   "打印任何传入的参数"
+   print ("输出: ")
+   print (arg1)
+   for var in vartuple:
+      print (var)
+   return;
+ 
+# 调用printinfo 函数
+printinfo( 10 );
+printinfo( 70, 60, 50 );
