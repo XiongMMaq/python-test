d66e7c335d493ea5470c40af7bd7d04d247c0a9f
XiongMM <xiongmm920@163.com>
2018-03-29 11:24:06 +0800

    路径获取方法

--- test_1.py
+++ test_1.py
@@ -4,3 +4,15 @@ flag = 0
 print(not flag)
 flag = 1
 print(not flag)
+
+
+import os,sys
+
+if __name__=="__main__":
+
+    #当前文件的路径
+    print("__file__ : {}".format(__file__))
+    #当前文件的目录
+    print("os.path.dirname(os.path.realpath(__file__)) : {}".format(os.path.dirname(os.path.realpath(__file__))))
+    #当前文件的目录
+    print("os.path.split(os.path.realpath(__file__)) : {}".format(os.path.split(os.path.realpath(__file__))[0]))
